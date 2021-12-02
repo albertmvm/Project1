34505899235d8ae2553d6cdd2f603a01b6ebeb20
amartinez@institutmvm.cat <amartinez@institutmvm.cat>
2021-12-02 13:27:18 +0100

    EJERCICIÓ NOMBRE I APELLIDOS

--- main.py
+++ main.py
@@ -2,11 +2,17 @@
 
 # Press Mayús+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 def main():
-    num=int(input("Introduce un número natural: "))
-    while num < 1:
-        num(int())
+
+    nom = str(input("Posa el teu nom: "))
+    cognom = str(input("Posa el teu cognom: "))
+    surname = str(input("Posa el teu segon cognom: "))
+    print("El teu nom és ", cognom[:2]+surname[:2]+nom[:2])
+
+
+if __name__ == '__main__':
+    main()
+
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
