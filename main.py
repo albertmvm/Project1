e8a208ecdd49f8cedee6763fe65113420b2f2fbf
amartinez@institutmvm.cat <amartinez@institutmvm.cat>
2021-12-02 12:43:01 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# This is a sample Python script.
+
+# Press Mayús+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def main():
+    num=int(input("Introduce un número natural: "))
+    while num < 1:
+        num(int())
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
